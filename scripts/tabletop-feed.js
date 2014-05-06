b6ee47febe21f0b888da5b17eb5347c98465e3d8
pts2td <pts2td@virginia.edu>
2014-05-05 23:07:10 -0400

    The skeleton of the Eddie Datatable Generator
    Version 0.42
    Some features were unable to be completed at the time of final
    submission

---
+++ scripts/tabletop-feed.js
@@ -0,0 +1,57 @@
+var jqueryNoConflict = jQuery;
+
+// begin main function
+jqueryNoConflict(document).ready(function(){
+   
+
+    initializeTabletopObject(key);
+
+});
+
+// pull data from google spreadsheet
+function initializeTabletopObject(dataSpreadsheet){
+    Tabletop.init({
+        key: dataSpreadsheet,
+        callback: writeTableWith,
+        simpleSheet: true,
+        debug: false
+    });
+}
+
+// create table headers
+function createTableColumns(){
+
+    
+    var cols = prompt("Enter the number of columns you with to display:");
+    var tableColumns = [cols];
+    for (var i = 1; i <= cols; i++){
+        tableColumns[i - 1] = {'mDataProp': 'col' + i , 'sTitle': 'Column ' + i , 'sClass': 'center'};
+    }
+   
+    return tableColumns;
+}
+
+// create the table container and object
+function writeTableWith(dataSource){
+
+    jqueryNoConflict('#demo').html('<table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered table-striped" id="data-table-container"></table>');
+
+    var oTable = jqueryNoConflict('#data-table-container').dataTable({
+		'sPaginationType': 'bootstrap',
+		'iDisplayLength': 25,
+        'aaData': dataSource,
+        'aoColumns': createTableColumns(),
+        'oLanguage': {
+            'sLengthMenu': '_MENU_ records per page'
+        }
+    });
+};
+
+//define two custom functions (asc and desc) for string sorting
+jQuery.fn.dataTableExt.oSort['string-case-asc']  = function(x,y) {
+	return ((x < y) ? -1 : ((x > y) ?  0 : 0));
+};
+
+jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
+	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+};
